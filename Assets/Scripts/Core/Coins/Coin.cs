073c9012c31be9817e916a4ce45c5c4c92e01993
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-07-14 15:03:27 -0400

    Coins

---
+++ Assets/Scripts/Core/Coins/Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public abstract class Coin : NetworkBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    protected int coinValue;
+    protected bool alreadyCollected;
+
+    public abstract int Collect();
+
+    public void SetValue(int value)
+    {
+        coinValue = value;
+    }
+
+    protected void Show(bool show)
+    {
+        spriteRenderer.enabled = show;
+    }
+}
---
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawningCoin : Coin
+{
+    public override int Collect()
+    {
+        if(!IsServer)
+        {
+            Show(false);
+            return 0;
+        }
+
+        if(alreadyCollected)
+        {
+            return 0;
+        }
+
+        alreadyCollected = true;
+        return coinValue;
+    }
+}
