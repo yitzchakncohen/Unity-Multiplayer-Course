e770f94aae3f8e9eb55c85c913acec34a06e1408
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-07-19 10:11:52 -0400

    Relay Service Setup

--- Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -55,14 +55,14 @@ public static class AuthenticationWrapper
                     break;
                 }
             }
-            catch (AuthenticationException ex)
+            catch (AuthenticationException authException)
             {
-                Debug.LogError(ex);
+                Debug.LogError(authException);
                 AuthState = AuthState.Error;
             }
-            catch (RequestFailedException ex)
+            catch (RequestFailedException requestException)
             {
-                Debug.LogError(ex);
+                Debug.LogError(requestException);
                 AuthState = AuthState.Error;
             }
 
