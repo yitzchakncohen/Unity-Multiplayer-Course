0c1ad877897f7406d21744f39ae550f1972e3aa0
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-07-23 15:43:09 -0400

    Respawning

--- Assets/Scripts/Core/Player/TankPlayer.cs
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -4,16 +4,21 @@ using Unity.Netcode;
 using UnityEngine;
 using Cinemachine;
 using Unity.Collections;
+using System;
 
 public class TankPlayer : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [field: SerializeField] public Health Health { get; private set; }
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
+    public static event Action<TankPlayer> OnPlayerSpawned;
+    public static event Action<TankPlayer> OnPlayerDespawned;
+
 
     public override void OnNetworkSpawn()
     {
@@ -24,11 +29,21 @@ public class TankPlayer : NetworkBehaviour
             // TODO add for dedicated host version
 
             PlayerName.Value = userData.userName;
+
+            OnPlayerSpawned?.Invoke(this);
         }
 
         if(IsOwner)
         {
             virtualCamera.Priority = ownerPriority;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if(IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this);
         }
     }
 }
