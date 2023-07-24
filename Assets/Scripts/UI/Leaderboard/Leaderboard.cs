70248852751bf2f5bc797e1c95018077be1ee1bd
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-07-24 09:56:48 -0400

    Leaderboard Names fix

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,6 +11,7 @@ public class Leaderboard : NetworkBehaviour
     [SerializeField] private LeaderboardEntityDisplay leaderboardEntityPrefab;
 
     private NetworkList<LeaderboardEntityState> leaderboardEntities;
+    private List<LeaderboardEntityDisplay> entityDisplays = new List<LeaderboardEntityDisplay>();
 
     private void Awake() 
     {
@@ -39,7 +41,7 @@ public class Leaderboard : NetworkBehaviour
             }
 
             TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
-            TankPlayer.OnPlayerSpawned += HandlePlayerDespawned;
+            TankPlayer.OnPlayerDespawned += HandlePlayerDespawned;
         }
     }
 
@@ -53,7 +55,7 @@ public class Leaderboard : NetworkBehaviour
         if(IsServer)
         {
             TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
-            TankPlayer.OnPlayerSpawned -= HandlePlayerDespawned;
+            TankPlayer.OnPlayerDespawned -= HandlePlayerDespawned;
         }
     }
 
@@ -62,13 +64,31 @@ public class Leaderboard : NetworkBehaviour
         switch (changeEvent.Type)
         {
             case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
-                LeaderboardEntityDisplay leaderboardEntity = Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                Debug.Log("add to leaderboard event " + changeEvent.Value.PlayerName);
+                if(!entityDisplays.Any(x => x.ClientId == changeEvent.Value.ClientId))
+                {
+                    Debug.Log("add to leaderboard "  + changeEvent.Value.PlayerName);
+                    LeaderboardEntityDisplay leaderboardEntity = Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                    leaderboardEntity.Initialize(changeEvent.Value.ClientId, changeEvent.Value.PlayerName, changeEvent.Value.Coins);
+                    entityDisplays.Add(leaderboardEntity);
+                }
                 break;
             case NetworkListEvent<LeaderboardEntityState>.EventType.Remove:
+                LeaderboardEntityDisplay displayToRemove = entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+                if(displayToRemove != null)
+                {
+                    displayToRemove.transform.SetParent(null);
+                    Destroy(displayToRemove.gameObject);
+                    entityDisplays.Remove(displayToRemove);
+                    Debug.Log("remove from leaderboard " + changeEvent.Value.PlayerName);
+                }
                 break;
             case NetworkListEvent<LeaderboardEntityState>.EventType.Value:
-                break;
-            default:
+                LeaderboardEntityDisplay displayToUpdate = entityDisplays.FirstOrDefault(x => x.ClientId == changeEvent.Value.ClientId);
+                if(displayToUpdate != null)
+                {
+                    displayToUpdate.UpdateCoins(changeEvent.Value.Coins);
+                }
                 break;
         }
     }
--- Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
 using UnityEngine;
 
 public class LeaderboardEntityDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text displayText;
+    private FixedString32Bytes playerName;
+    public ulong ClientId { get; private set;}
+    public int Coins { get; private set;}
+
+    public void Initialize(ulong clientId, FixedString32Bytes playerName, int coins)
     {
+        this.ClientId = clientId;
+        this.playerName = playerName;
         
+        UpdateCoins(coins);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateCoins(int coins)
     {
+        this.Coins = coins;
         
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        displayText.text = $"1. {playerName} ({this.Coins})";
     }
 }
