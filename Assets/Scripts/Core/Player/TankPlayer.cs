b15204d4db119c2e76ff4e7b387050b5c9619a30
Yitzchak Cohen <yitzchakncohen@gmail.com>
2023-08-09 16:53:05 -0400

    Matchmaking UI

--- Assets/Scripts/Core/Player/TankPlayer.cs
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -11,6 +11,7 @@ public class TankPlayer : NetworkBehaviour
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private SpriteRenderer minimapIconRenderer;
+    [SerializeField] private Texture2D crosshair;
     [field: SerializeField] public Health Health { get; private set; }
     [field: SerializeField] public CoinWallet Wallet { get; private set; }
 
@@ -41,6 +42,7 @@ public class TankPlayer : NetworkBehaviour
         {
             virtualCamera.Priority = ownerPriority;
             minimapIconRenderer.color = ownerColour;
+            Cursor.SetCursor(crosshair, new Vector2(crosshair.width/2, crosshair.height/2), CursorMode.Auto);
         }
     }
 
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text queueTimerText;
+    [SerializeField] private TMP_Text queueStatusText;
+    [SerializeField] private TMP_Text findMatchButtonText;
     [SerializeField] private TMP_InputField joinCodeField;
+
+    private void Start() 
+    {
+        if(ClientSingleton.Instance == null) { return; } 
+
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+
+        queueStatusText.text = string.Empty;
+        queueTimerText.text = string.Empty; 
+    }
 
     public async void StartHost()
     {
